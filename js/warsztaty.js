a2798d5b53985fb84b14fa7d12494f4c947ef038
arcontact <helpdesk@arcontact.pl>
2014-05-30 12:56:39 +0200

    kw

--- js/warsztaty.js
+++ js/warsztaty.js
@@ -113,7 +113,7 @@ function getNews() {
 			if(i%per_page==0 && i!=0){
 				out = out + '</ul></div><div class="news"><ul data-ajax="false" data-inset="true">';
 			}
-			out = out + '<li><a href="#" data-ajax="false" onclick="window.open('+item.link+', \'_blank\', \'location=yes\');"><h6>' + item.title + '</h6><span>' +item.date+ '</span></a></li>';
+			out = out + '<li><a href="#" data-ajax="false" onclick="window.open(\''+item.link+'\', \'_blank\', \'location=yes\');"><h6>' + item.title + '</h6><span>' +item.date+ '</span></a></li>';
 		});
 		out = out + '</div>';
 		
