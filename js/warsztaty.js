0d241787b906b636613798bd9b88a5097dfba0e6
arcontact <helpdesk@arcontact.pl>
2014-06-03 08:26:47 +0200

    *

--- js/warsztaty.js
+++ js/warsztaty.js
@@ -1,6 +1,26 @@
 var newsUrl = 'http://www.q-service.com.pl/rss/';
 
 $.mobile.defaultPageTransition = 'none';
+document.addEventListener("deviceready", onDeviceReady, false);
+
+function onDeviceReady() {
+	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+
+}
+function gotFS(fileSystem) {
+	var path = "readme.txt";
+	fileSystem.root.getFile(path, {create: true, exclusive: false}, gotFileEntry, fail);
+}
+function gotFileEntry(fileEntry) {
+	fileEntry.createWriter(gotFileWriter, fail);
+}
+
+function gotFileWriter(writer) {
+	writer.onwrite = function(evt) {
+		console.log("write success");
+	};
+	writer.write("some sample text");
+}
 
 $(window).load(function(){
 	$(".main-loader").hide();
