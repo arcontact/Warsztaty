191311b100d5fa2b103804254ef18f03816ffa96
arcontact <helpdesk@arcontact.pl>
2014-06-03 11:49:08 +0200

    1

--- js/warsztaty.js
+++ js/warsztaty.js
@@ -5,11 +5,22 @@ var w_path = 'wdata.json';
 document.addEventListener("deviceready", onDeviceReady, fail);
 
 function onDeviceReady() {
-	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
+	$('body').text('Ładowanie danych...');
+	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFSWrite, fail);
 }
-function gotFS(fileSystem) {	
+
+function gotFSRead(fileSystem) {	
+	fileSystem.root.getFile(w_path, {create: false}, gotFileEntryRead, fail);
+}
+
+function gotFSWrite(fileSystem) {	
 	fileSystem.root.getFile(w_path, {create: true, exclusive: false}, gotFileEntry, fail);
 }
+
+function gotFileEntryRead(fileEntry) {
+	fileEntry.file(readFile, fail);
+}
+
 function gotFileEntry(fileEntry) {
 	fileEntry.createWriter(gotFileWriter, fail);
 }
@@ -17,11 +28,10 @@ function gotFileEntry(fileEntry) {
 function gotFileWriter(writer) {
 	writer.onwrite = function(evt) {
 		console.log(evt);
-		$('body').text('Dane zostały załadowane.');
+		$('body').text('Dane zostały zapisane do pliku.');
 	};
 	
 	if(navigator.onLine) {
-		$('body').text('Ładowanie danych...');
 		$.ajax({
 			type: 'GET',
 			url: dataUrl,
@@ -29,12 +39,24 @@ function gotFileWriter(writer) {
 			data: {type: 'list'},
 			success: function(dat){
 				writer.write(dat);
+				//wyswietlanie...
 			},
 			error: function(xhr, type){
 				alert('Ajax error!!!')
 			}
 		});
+	} else {
+		window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFSRead, fail);
 	}
+}
+
+function readFile(file) {
+	var reader = new FileReader();
+	reader.onloadend = function(evt) {
+		console.log("Read as text");
+		console.log(evt.target.result);
+	};
+	reader.readAsText(file);
 }
 
 function fail(err) {
