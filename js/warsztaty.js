c9d510f9f4c641874ea86a77ae1fe0dc2e7d3cd0
arcontact <helpdesk@arcontact.pl>
2014-06-03 10:22:07 +0200

    1

--- js/warsztaty.js
+++ js/warsztaty.js
@@ -5,7 +5,7 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
 	if(navigator.onLine) {
 		$.get(newsUrl, function(response){
-			alert(response.find('item').eq(0).text());
+			alert(response.html());
 		});
 	}
 	//window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, gotFS, fail);
